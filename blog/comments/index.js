c311ef49445644f8ae6b395d47dc25319d97765d
bosnjaknikola <nikola.bosnjak@wayseven.com>
2020-05-12 16:39:17 +0200

    working on posts

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const { randomBytes } = require("crypto");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
-app.get("/posts/:id/comments", (req, res) => {});
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id]);
+});
 
 app.post("/posts/:id/comments", (req, res) => {
   const commentId = randomBytes(4).toString("hex");
@@ -19,6 +23,8 @@ app.post("/posts/:id/comments", (req, res) => {
   comments.push({ id: commentId, content });
 
   commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
 });
 
 app.listen(4001, console.log("Listening on 4001"));
